1d5337e57b6fce94cd7716d7642a4851e0c4ac9d
Sijie Fu <fusijie@cmu.edu>
2022-04-25 12:29:21 -0400

    Update utils.py

--- baselines/datasets/utils.py
+++ baselines/datasets/utils.py
@@ -215,38 +215,44 @@ def parse_lines(lines, files, fn):
 def split_files(split_path, files, args):
     overal_path = split_path
     split_path = "../" + overal_path
+    
     if args.datasetSplitDone:
         print(f'Gathering dataset split information from {overal_path}',flush=True)
-        with open(split_path+"valid_ids.txt", 'r') as f:
-            valid_ids = parse_lines(lines=f.readlines(), files=files, fn=overal_path+"valid_ids.txt")
-        with open(split_path+"test_ids.txt", 'r') as f:
-            test_ids = parse_lines(lines=f.readlines(), files=files, fn=overal_path+"test_ids.txt")
-        with open(split_path+"train_ids.txt", 'r') as f:
-            train_ids = parse_lines(lines=f.readlines(), files=files, fn=overal_path+"train_ids.txt")
-    else:
-        n_valid, n_test = args.splitRatio.split('_')
-        try:
-            n_valid = int(n_valid)
-        except:
-            n_valid = int(float(n_valid) * len(files))
-        try:
-            n_test = int(n_test)
-        except:
-            n_test = int(float(n_test) * len(files))
 
-        print(f'Split configuration: train({len(files)-n_valid-n_test}), valid ({n_valid}), test({n_test})')
-        idx = np.random.permutation(len(files))
-        idx = idx.tolist()    
-        valid_ids = [files[i] for i in idx[0:n_valid]]
-        test_ids = [files[i] for i in idx[n_valid:n_valid+n_test]]
-        train_ids = [files[i] for i in idx[n_valid+n_test:]]
+        try:
+            with open(split_path+"valid_ids.txt", 'r') as f:
+                valid_ids = parse_lines(lines=f.readlines(), files=files, fn=overal_path+"valid_ids.txt")
+            with open(split_path+"test_ids.txt", 'r') as f:
+                test_ids = parse_lines(lines=f.readlines(), files=files, fn=overal_path+"test_ids.txt")
+            with open(split_path+"train_ids.txt", 'r') as f:
+                train_ids = parse_lines(lines=f.readlines(), files=files, fn=overal_path+"train_ids.txt")
+            return valid_ids, test_ids, train_ids
+        except:
+            print(f"No pre-defined split information found. New split info will be used instead.", flush=True)
+    
+    n_valid, n_test = args.splitRatio.split('_')
+    try:
+        n_valid = int(n_valid)
+    except:
+        n_valid = int(float(n_valid) * len(files))
+    try:
+        n_test = int(n_test)
+    except:
+        n_test = int(float(n_test) * len(files))
+    
+    print(f'Creating new split configuration: train({len(files)-n_valid-n_test}), valid ({n_valid}), test({n_test})')
+    idx = np.random.permutation(len(files))
+    idx = idx.tolist()    
+    valid_ids = [files[i] for i in idx[0:n_valid]]
+    test_ids = [files[i] for i in idx[n_valid:n_valid+n_test]]
+    train_ids = [files[i] for i in idx[n_valid+n_test:]]
 
-        with open(split_path+"valid_ids.txt", "w") as f:
-            f.write("\n".join(valid_ids)+"\n")
-        with open(split_path+"test_ids.txt", "w") as f:
-            f.write("\n".join(test_ids)+"\n")
-        with open(split_path+"train_ids.txt", "w") as f:
-            f.write("\n".join(train_ids)+"\n")
-        print(f"Split configuration is dumped in {overal_path}",flush=True)
+    with open(split_path+"valid_ids.txt", "w") as f:
+        f.write("\n".join(valid_ids)+"\n")
+    with open(split_path+"test_ids.txt", "w") as f:
+        f.write("\n".join(test_ids)+"\n")
+    with open(split_path+"train_ids.txt", "w") as f:
+        f.write("\n".join(train_ids)+"\n")
+    print(f"Split configuration is dumped in {overal_path}",flush=True)
     
     return valid_ids, test_ids, train_ids
