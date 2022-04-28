c04419a111f61a9031d7ca8fbb6bc623ff927b99
Sijie Fu <fusijie@cmu.edu>
2022-04-28 13:15:03 -0400

    scripts

--- GraphFunctions.py
+++ GraphFunctions.py
@@ -1,8 +1,12 @@
-from models import NNet, MPNN
+import os, argparse
 
+import numpy as np
 import torch
 import torch.nn as nn
 from torch.autograd.variable import Variable
+import torch.nn.functional as F
+
+from models import NNet, MPNN
 
 __author__ = "Pau Riba, Anjan Dutta"
 __email__ = "priba@cvc.uab.cat, adutta@cvc.uab.cat" 
@@ -11,7 +15,7 @@ __email__ = "priba@cvc.uab.cat, adutta@cvc.uab.cat"
 class MessageFunction(nn.Module):
 
     # Constructor
-    def __init__(self, message_def='duvenaud', args={}):
+    def __init__(self, message_def='mpnn', args={}):
         super(MessageFunction, self).__init__()
         self.m_definition = ''
         self.m_function = None
@@ -30,11 +34,7 @@ class MessageFunction(nn.Module):
                     'duvenaud':         self.m_duvenaud,
                     'ggnn':             self.m_ggnn,
                     'intnet':           self.m_intnet,
-                    'mpnn':             self.m_mpnn,
-                    'mgc':              self.m_mgc,
-                    'bruna':            self.m_bruna,
-                    'defferrard':       self.m_deff,
-                    'kipf':             self.m_kipf
+                    'mpnn':             self.m_mpnn
                 }.get(self.m_definition, None)
 
         if self.m_function is None:
@@ -177,7 +177,290 @@ class MessageFunction(nn.Module):
 
         return nn.ParameterList(learn_args), nn.ModuleList(learn_modules), args
 
-    # Kearnes et al. (2016), Molecular Graph Convolutions
-    def m_mgc(self, h_v, h_w, e_vw, args):
-        m = e_vw
-        return m
+
+class UpdateFunction(nn.Module):
+
+    # Constructor
+    def __init__(self, update_def='mpnn', args={}):
+        super(UpdateFunction, self).__init__()
+        self.u_definition = ''
+        self.u_function = None
+        self.args = {}
+        self.__set_update(update_def, args)
+
+    # Update node hv given message mv
+    def forward(self, h_v, m_v, opt={}):
+        return self.u_function(h_v, m_v, opt)
+
+    # Set update function
+    def __set_update(self, update_def, args):
+        self.u_definition = update_def.lower()
+
+        self.u_function = {
+                    'duvenaud':         self.u_duvenaud,
+                    'ggnn':             self.u_ggnn,
+                    'intnet':           self.u_intnet,
+                    'mpnn':             self.u_mpnn
+                }.get(self.u_definition, None)
+
+        if self.u_function is None:
+            print('WARNING!: Update Function has not been set correctly\n\tIncorrect definition ' + update_def)
+
+        init_parameters = {
+            'duvenaud':         self.init_duvenaud,
+            'ggnn':             self.init_ggnn,
+            'intnet':           self.init_intnet,
+            'mpnn':             self.init_mpnn
+        }.get(self.u_definition, lambda x: (nn.ParameterList([]), nn.ModuleList([]), {}))
+
+        self.learn_args, self.learn_modules, self.args = init_parameters(args)
+
+    # Get the name of the used update function
+    def get_definition(self):
+        return self.u_definition
+
+    # Get the update function arguments
+    def get_args(self):
+        return self.args
+
+    ## Definition of various state of the art update functions
+
+    # Duvenaud
+    def u_duvenaud(self, h_v, m_v, opt):
+
+        param_sz = self.learn_args[0][opt['deg']].size()
+        parameter_mat = torch.t(self.learn_args[0][opt['deg']])[None, ...].expand(m_v.size(0), param_sz[1], param_sz[0])
+
+        aux = torch.bmm(parameter_mat, torch.transpose(m_v, 1, 2))
+
+        return torch.transpose(torch.nn.Sigmoid()(aux), 1, 2)
+
+    def init_duvenaud(self, params):
+        learn_args = []
+        learn_modules = []
+        args = {}
+
+        # Filter degree 0 (the message will be 0 and therefore there is no update
+        args['deg'] = [i for i in params['deg'] if i!=0]
+        args['in'] = params['in']
+        args['out'] = params['out']
+
+        # Define a parameter matrix H for each degree.
+        learn_args.append(torch.nn.Parameter(torch.randn(len(args['deg']), args['in'], args['out'])))
+
+        return nn.ParameterList(learn_args), nn.ModuleList(learn_modules), args
+
+    # GG-NN, Li et al.
+    def u_ggnn(self, h_v, m_v, opt={}):
+        h_v.contiguous()
+        m_v.contiguous()
+        h_new = self.learn_modules[0](torch.transpose(m_v, 0, 1), torch.unsqueeze(h_v, 0))[0]  # 0 or 1???
+        return torch.transpose(h_new, 0, 1)
+
+    def init_ggnn(self, params):
+        learn_args = []
+        learn_modules = []
+        args = {}
+
+        args['in_m'] = params['in_m']
+        args['out'] = params['out']
+
+        # GRU
+        learn_modules.append(nn.GRU(params['in_m'], params['out']))
+
+        return nn.ParameterList(learn_args), nn.ModuleList(learn_modules), args
+
+    # Battaglia et al. (2016), Interaction Networks
+    def u_intnet(self, h_v, m_v, opt):
+        if opt['x_v'].ndimension():
+            input_tensor = torch.cat([h_v, opt['x_v'], torch.squeeze(m_v)], 1)
+        else:
+            input_tensor = torch.cat([h_v, torch.squeeze(m_v)], 1)
+
+        return self.learn_modules[0](input_tensor)
+
+    def init_intnet(self, params):
+        learn_args = []
+        learn_modules = []
+        args = {}
+
+        args['in'] = params['in']
+        args['out'] = params['out']
+
+        learn_modules.append(NNet(n_in=params['in'], n_out=params['out']))
+
+        return nn.ParameterList(learn_args), nn.ModuleList(learn_modules), args
+
+    def u_mpnn(self, h_v, m_v, opt={}):
+        h_in = h_v.view(-1,h_v.size(2))
+        m_in = m_v.view(-1,m_v.size(2))
+        h_new = self.learn_modules[0](m_in[None,...],h_in[None,...])[0] # 0 or 1???
+        return torch.squeeze(h_new).view(h_v.size())
+
+    def init_mpnn(self, params):
+        learn_args = []
+        learn_modules = []
+        args = {}
+
+        args['in_m'] = params['in_m']
+        args['out'] = params['out']
+
+        # GRU
+        learn_modules.append(nn.GRU(params['in_m'], params['out']))
+
+        return nn.ParameterList(learn_args), nn.ModuleList(learn_modules), args
+
+
+class ReadoutFunction(nn.Module):
+
+    # Constructor
+    def __init__(self, readout_def='mpnn', args={}):
+        super(ReadoutFunction, self).__init__()
+        self.r_definition = ''
+        self.r_function = None
+        self.args = {}
+        self.__set_readout(readout_def, args)
+
+    # Readout graph given node values at las layer
+    def forward(self, h_v):
+        return self.r_function(h_v)
+
+    # Set a readout function
+    def __set_readout(self, readout_def, args):
+        self.r_definition = readout_def.lower()
+
+        self.r_function = {
+                    'duvenaud': self.r_duvenaud,
+                    'ggnn':     self.r_ggnn,
+                    'intnet':   self.r_intnet,
+                    'mpnn':     self.r_mpnn
+                }.get(self.r_definition, None)
+
+        if self.r_function is None:
+            print('WARNING!: Readout Function has not been set correctly\n\tIncorrect definition ' + readout_def)
+            quit()
+
+        init_parameters = {
+            'duvenaud': self.init_duvenaud,
+            'ggnn':     self.init_ggnn,
+            'intnet':   self.init_intnet,
+            'mpnn':     self.init_mpnn
+        }.get(self.r_definition, lambda x: (nn.ParameterList([]), nn.ModuleList([]), {}))
+
+        self.learn_args, self.learn_modules, self.args = init_parameters(args)
+
+    # Get the name of the used readout function
+    def get_definition(self):
+        return self.r_definition
+
+    ## Definition of various state of the art update functions
+
+    # Duvenaud
+    def r_duvenaud(self, h):
+        # layers
+        aux = []
+        for l in range(len(h)):
+            param_sz = self.learn_args[l].size()
+            parameter_mat = torch.t(self.learn_args[l])[None, ...].expand(h[l].size(0), param_sz[1],
+                                                                                      param_sz[0])
+
+            aux.append(torch.transpose(torch.bmm(parameter_mat, torch.transpose(h[l], 1, 2)), 1, 2))
+
+            for j in range(0, aux[l].size(1)):
+                # Mask whole 0 vectors
+                aux[l][:, j, :] = nn.Softmax()(aux[l][:, j, :].clone())*(torch.sum(aux[l][:, j, :] != 0, 1) > 0).expand_as(aux[l][:, j, :]).type_as(aux[l])
+
+        aux = torch.sum(torch.sum(torch.stack(aux, 3), 3), 1)
+        return self.learn_modules[0](torch.squeeze(aux))
+
+    def init_duvenaud(self, params):
+        learn_args = []
+        learn_modules = []
+        args = {}
+
+        args['out'] = params['out']
+
+        # Define a parameter matrix W for each layer.
+        for l in range(params['layers']):
+            learn_args.append(nn.Parameter(torch.randn(params['in'][l], params['out'])))
+
+        # learn_modules.append(nn.Linear(params['out'], params['target']))
+
+        learn_modules.append(NNet(n_in=params['out'], n_out=params['target']))
+        return nn.ParameterList(learn_args), nn.ModuleList(learn_modules), args
+
+    # GG-NN, Li et al.
+    def r_ggnn(self, h):
+
+        aux = Variable( torch.Tensor(h[0].size(0), self.args['out']).type_as(h[0].data).zero_() )
+        # For each graph
+        for i in range(h[0].size(0)):
+            nn_res = nn.Sigmoid()(self.learn_modules[0](torch.cat([h[0][i,:,:], h[-1][i,:,:]], 1)))*self.learn_modules[1](h[-1][i,:,:])
+
+            # Delete virtual nodes
+            nn_res = (torch.sum(h[0][i,:,:],1).expand_as(nn_res)>0).type_as(nn_res)* nn_res
+
+            aux[i,:] = torch.sum(nn_res,0)
+
+        return aux
+
+    def init_ggnn(self, params):
+        learn_args = []
+        learn_modules = []
+        args = {}
+
+        # i
+        learn_modules.append(NNet(n_in=2*params['in'], n_out=params['target']))
+
+        # j
+        learn_modules.append(NNet(n_in=params['in'], n_out=params['target']))
+
+        args['out'] = params['target']
+
+        return nn.ParameterList(learn_args), nn.ModuleList(learn_modules), args
+
+
+    # Battaglia et al. (2016), Interaction Networks
+    def r_intnet(self, h):
+
+        aux = torch.sum(h[-1],1)
+
+        return self.learn_modules[0](aux)
+
+    def init_intnet(self, params):
+        learn_args = []
+        learn_modules = []
+        args = {}
+
+        learn_modules.append(NNet(n_in=params['in'], n_out=params['target']))
+
+        return nn.ParameterList(learn_args), nn.ModuleList(learn_modules), args
+
+    def r_mpnn(self, h):
+
+        aux = Variable( torch.Tensor(h[0].size(0), self.args['out']).type_as(h[0].data).zero_() )
+        # For each graph
+        for i in range(h[0].size(0)):
+            nn_res = nn.Sigmoid()(self.learn_modules[0](torch.cat([h[0][i,:,:], h[-1][i,:,:]], 1)))*self.learn_modules[1](h[-1][i,:,:])
+
+            # Delete virtual nodes
+            nn_res = (torch.sum(h[0][i,:,:],1)[...,None].expand_as(nn_res)>0).type_as(nn_res)* nn_res
+
+            aux[i,:] = torch.sum(nn_res,0)
+
+        return aux
+
+    def init_mpnn(self, params):
+        learn_args = []
+        learn_modules = []
+        args = {}
+
+        # i
+        learn_modules.append(NNet(n_in=2*params['in'], n_out=params['target']))
+
+        # j
+        learn_modules.append(NNet(n_in=params['in'], n_out=params['target']))
+
+        args['out'] = params['target']
+
+        return nn.ParameterList(learn_args), nn.ModuleList(learn_modules), args
--- models.py
+++ models.py
@@ -111,3 +111,264 @@ class MPNN(nn.Module):
         if self.type == 'classification':
             res = nn.LogSoftmax()(res)
         return res
+
+
+class MpnnGGNN(nn.Module):
+    """
+        MPNN as proposed by Li et al..
+
+        This class implements the whole Li et al. model following the functions proposed by Gilmer et al. as
+        Message, Update and Readout.
+
+        Parameters
+        ----------
+        e : int list.
+            Possible edge labels for the input graph.
+        hidden_state_size : int
+            Size of the hidden states (the input will be padded with 0's to this size).
+        message_size : int
+            Message function output vector size.
+        n_layers : int
+            Number of iterations Message+Update (weight tying).
+        l_target : int
+            Size of the output.
+        type : str (Optional)
+            Classification | [Regression (default)]. If classification, LogSoftmax layer is applied to the output vector.
+    """
+
+    def __init__(self, e, hidden_state_size, message_size, n_layers, l_target, type='regression'):
+        super(MpnnGGNN, self).__init__()
+
+        # Define message
+        self.m = nn.ModuleList([MessageFunction('ggnn', args={'e_label': e, 'in': hidden_state_size, 'out': message_size})])
+
+        # Define Update
+        self.u = nn.ModuleList([UpdateFunction('ggnn',
+                                                args={'in_m': message_size,
+                                                'out': hidden_state_size})])
+
+        # Define Readout
+        self.r = ReadoutFunction('ggnn',
+                                 args={'in': hidden_state_size,
+                                       'target': l_target})
+
+        self.type = type
+
+        self.args = {}
+        self.args['out'] = hidden_state_size
+
+        self.n_layers = n_layers
+
+    def forward(self, g, h_in, e):
+
+        h = []
+
+        # Padding to some larger dimension d
+        h_t = torch.cat([h_in, Variable(torch.Tensor(h_in.size(0), h_in.size(1), self.args['out'] - h_in.size(2)).type_as(h_in.data).zero_())], 2)
+
+        h.append(h_t.clone())
+
+        # Layer
+        for t in range(0, self.n_layers):
+
+            h_t = Variable(torch.zeros(h[0].size(0), h[0].size(1), h[0].size(2)).type_as(h_in.data))
+
+            # Apply one layer pass (Message + Update)
+            for v in range(0, h_in.size(1)):
+
+                m = self.m[0].forward(h[t][:, v, :], h[t], e[:, v, :])
+
+                # Nodes without edge set message to 0
+                m = g[:, v, :, None].expand_as(m) * m
+
+                m = torch.sum(m, 1)
+
+                # Update
+                h_t[:, v, :] = self.u[0].forward(h[t][:, v, :], m)
+
+            # Delete virtual nodes
+            h_t = (torch.sum(torch.abs(h_in), 2).expand_as(h_t) > 0).type_as(h_t)*h_t
+            h.append(h_t.clone())
+
+        # Readout
+        res = self.r.forward(h)
+        if self.type == 'classification':
+            res = nn.LogSoftmax()(res)
+        return res
+
+
+class MpnnIntNet(nn.Module):
+    """
+        MPNN as proposed by Battaglia et al..
+
+        This class implements the whole Battaglia et al. model following the functions proposed by Gilmer et al. as
+        Message, Update and Readout.
+
+        Parameters
+        ----------
+        in_n : int list
+            Sizes for the node and edge features.
+        out_message : int list
+            Output sizes for the different Message functions.
+        out_update : int list
+            Output sizes for the different Update functions.
+        l_target : int
+            Size of the output.
+        type : str (Optional)
+            Classification | [Regression (default)]. If classification, LogSoftmax layer is applied to the output vector.
+    """
+
+    def __init__(self, in_n, out_message, out_update, l_target, type='regression'):
+        super(MpnnIntNet, self).__init__()
+
+        n_layers = len(out_update)
+
+        # Define message 1 & 2
+        self.m = nn.ModuleList([MessageFunction('intnet', args={'in': 2*in_n[0] + in_n[1], 'out': out_message[i]})
+                                if i == 0 else
+                                MessageFunction('intnet', args={'in': 2*out_update[i-1] + in_n[1], 'out': out_message[i]})
+                                for i in range(n_layers)])
+
+        # Define Update 1 & 2
+        self.u = nn.ModuleList([UpdateFunction('intnet', args={'in': in_n[0]+out_message[i], 'out': out_update[i]})
+                                if i == 0 else
+                                UpdateFunction('intnet', args={'in': out_update[i-1]+out_message[i], 'out': out_update[i]})
+                                for i in range(n_layers)])
+
+        # Define Readout
+        self.r = ReadoutFunction('intnet', args={'in': out_update[-1], 'target': l_target})
+
+        self.type = type
+
+    def forward(self, g, h_in, e):
+
+        h = []
+        h.append(h_in)
+
+        # Layer
+        for t in range(0, len(self.m)):
+
+            u_args = self.u[t].get_args()
+            h_t = Variable(torch.zeros(h_in.size(0), h_in.size(1), u_args['out']).type_as(h[t].data))
+
+            # Apply one layer pass (Message + Update)
+            for v in range(0, h_in.size(1)):
+
+                m = self.m[t].forward(h[t][:, v, :], h[t], e[:, v, :, :])
+
+                # Nodes without edge set message to 0
+                m = g[:, v, :,None].expand_as(m) * m
+
+                m = torch.sum(m, 1)
+
+                # Interaction Net
+                opt = {}
+                opt['x_v'] = Variable(torch.Tensor([]).type_as(m.data))
+
+                h_t[:, v, :] = self.u[t].forward(h[t][:, v, :], m, opt)
+
+            h.append(h_t.clone())
+
+        # Readout
+        res = self.r.forward(h)
+        if self.type == 'classification':
+            res = nn.LogSoftmax()(res)
+        return res
+
+
+class MpnnDuvenaud(nn.Module):
+    """
+        MPNN as proposed by Duvenaud et al..
+
+        This class implements the whole Duvenaud et al. model following the functions proposed by Gilmer et al. as 
+        Message, Update and Readout.
+
+        Parameters
+        ----------
+        d : int list.
+            Possible degrees for the input graph.
+        in_n : int list
+            Sizes for the node and edge features.
+        out_update : int list
+            Output sizes for the different Update functions.
+        hidden_state_readout : int
+            Input size for the neural net used inside the readout function.
+        l_target : int
+            Size of the output.
+        type : str (Optional)
+            Classification | [Regression (default)]. If classification, LogSoftmax layer is applied to the output vector.
+    """
+
+    def __init__(self, d, in_n, out_update, hidden_state_readout, l_target, type='regression'):
+        super(MpnnDuvenaud, self).__init__()
+
+        n_layers = len(out_update)
+
+        # Define message 1 & 2
+        self.m = nn.ModuleList([MessageFunction('duvenaud') for _ in range(n_layers)])
+
+        # Define Update 1 & 2
+        self.u = nn.ModuleList([UpdateFunction('duvenaud', args={'deg': d, 'in': self.m[i].get_out_size(in_n[0], in_n[1]), 'out': out_update[0]}) if i == 0 else
+                                UpdateFunction('duvenaud', args={'deg': d, 'in': self.m[i].get_out_size(out_update[i-1], in_n[1]), 'out': out_update[i]}) for i in range(n_layers)])
+
+        # Define Readout
+        self.r = ReadoutFunction('duvenaud',
+                                 args={'layers': len(self.m) + 1,
+                                       'in': [in_n[0] if i == 0 else out_update[i-1] for i in range(n_layers+1)],
+                                       'out': hidden_state_readout,
+                                       'target': l_target})
+
+        self.type = type
+
+    def forward(self, g, h_in, e, plotter=None):
+
+        h = []
+        h.append(h_in)
+
+        # Layer
+        for t in range(0, len(self.m)):
+
+            u_args = self.u[t].get_args()
+
+            h_t = Variable(torch.zeros(h_in.size(0), h_in.size(1), u_args['out']).type_as(h[t].data))
+
+            # Apply one layer pass (Message + Update)
+            for v in range(0, h_in.size(1)):
+
+                m = self.m[t].forward(h[t][:, v, :], h[t], e[:, v, :])
+
+                # Nodes without edge set message to 0
+                m = g[:, v, :, None].expand_as(m) * m
+
+                m = torch.sum(m, 1)
+
+                # Duvenaud
+                deg = torch.sum(g[:, v, :].data, 1)
+
+                # Separate degrees
+                for i in range(len(u_args['deg'])):
+                    ind = deg == u_args['deg'][i]
+                    ind = Variable(torch.squeeze(torch.nonzero(torch.squeeze(ind))), volatile=True)
+
+                    opt = {'deg': i}
+
+                    # Update
+                    if len(ind) != 0:
+                        aux = self.u[t].forward(torch.index_select(h[t], 0, ind)[:, v, :], torch.index_select(m, 0, ind), opt)
+
+                        ind = ind.data.cpu().numpy()
+                        for j in range(len(ind)):
+                            h_t[ind[j], v, :] = aux[j, :]
+
+            if plotter is not None:
+                num_feat = h_t.size(2)
+                color = h_t[0,:,:].data.cpu().numpy()
+                for i in range(num_feat):
+                    plotter(color[:, i], 'layer_' + str(t) + '_element_' + str(i) + '.png')
+
+            h.append(h_t.clone())
+        # Readout
+        res = self.r.forward(h)
+        if self.type == 'classification':
+            res = nn.LogSoftmax()(res)
+        return res
